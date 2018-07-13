855a5a7d4c66e577b2f6172527330770a6d89f99
Jared Thibault <jared.lee.thibault@gmail.com>
2018-07-13 13:17:41 -0400

    separated scripts and other stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+# scripts
+Mostly my custom scripts (what isn't mine is quarantined). Most are just to get some basic functionality working.
---
+++ scripts/octave.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###
+#
+# Octave Script
+#
+###
+
+# Descrption:
+#	Literally just launches "octave --force-gui --traditional"
+
+octave --force-gui --traditional
